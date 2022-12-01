93002f278d0ae28d642b43ba3bf5864440c71b1a
Lucas Zanardo <lucas.zanardo.etu@univ-lille.fr>
2022-12-01 20:26:29 +0100

    <fix> added fullscreen canvas

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,8 @@
 //Create a Pixi Application
-const app = new PIXI.Application({width: 256, height: 256});
+const app = new PIXI.Application({
+    width: screen.width,
+    height: screen.height
+});
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
