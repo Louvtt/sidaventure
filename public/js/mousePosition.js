68a526f06fd7f811b582119062f0cf6a81b988f4
--add <yassin.amazrayin.etu@univ-lille.fr>
2022-12-02 00:56:27 +0100

    js a jour

--- public/js/mousePosition.js
+++ public/js/mousePosition.js
@@ -1,40 +1,73 @@
 
-<script>
 var object = document.getElementById("object");
 
-var xMousePos = 0;
-var yMousePos = 0;
-var posklex=10;
-var poskley=10;
-document.onmousemove = function(e)
-{
-  xMousePos = e.clientX + window.pageXOffset;
-  yMousePos = e.clientY + window.pageYOffset;
+const mousePos = {
+  current: {x: 0, y: 0},
+  last: []
 };
 
-function update(e)
+var posklex=10;
+var poskley=10;
+document.onmousemove = calculateMouseDelta;
+
+function calculateMouseDelta(e) {
+  mousePos.last.push({ ...mousePos.current});
+  if(mousePos.last.length > 10) {
+    for (let index = 0; index < Math.max(0, mousePos.last.length-10); index++) {
+      mousePos.last.shift();      
+    }
+  }
+
+  let moyDeltaX = 0;
+  let moyDeltaY = 0;
+  for (let i = 1; i < mousePos.last.length; i++) {
+    const pos1 = mousePos.last[i];
+    const pos2 = mousePos.last[i-1];
+    const deltaX = pos1.x - pos2.x;
+    const deltaY = pos1.y - pos2.y;
+    moyDeltaX += deltaX;
+    moyDeltaY += deltaY;
+  }
+  moyDeltaX /= mousePos.last.length;
+  moyDeltaY /= mousePos.last.length;
+
+  mousePos.current.x = e.clientX;
+  mousePos.current.y = e.clientY;
+
+  // calculate delta
+  const deltaX = moyDeltaX;
+  const deltaY = moyDeltaY;
+
+  if(deltaX > deltaY) {
+    console.log("h");
+  } else if(deltaX < deltaY) {
+    console.log("v");
+  } else {
+    console.log("static");
+  }
+}
+
+function testKey(e)
 {
-if (klé== false){
-    alert("il faut la clé pour ouvrir);
+  if (!klé)
+    alert("il faut la clé pour ouvrir");
+  //action pour la porte 
+  if(klé)
+      alert("la porte est ouverte")
 }
-if(klé== true )
-    //action pour la porte 
-    alert("la porte est ouverte")
-}
 
-object.onclick = update;
+object.onclick = testKey;
 
-object.onmousemove = function(){
-  var x = xMousePos - object.offsetLeft;
-  var y = yMousePos - object.offsetTop;
-  cursor1: url('...') x y, url('...') x y, défaut;// importer l'image d'une klé
-  object.style.cursor='wait'
-  if(x > object.offsetWidth - 4)//position et cursor a modifier en fonction de la position
-    object.style.cursor='e-resize';
-  if(y > object.offsetHeight - 4)//position et cursor a modifier en fonction de la position
-    object.style.cursor='n-resize';
-if (x=posklex and y=poskley)
-    object.style.cursor=cursor1
+// object.onmousemove = function(){
+//   var x = xMousePos - object.offsetLeft;
+//   var y = yMousePos - object.offsetTop;
+//   //cursor1: url('...'), défaut;// importer l'image d'une klé
+//   object.style.cursor='wait'
+//   if(x > object.offsetWidth - 4)//position et cursor a modifier en fonction de la position
+//     object.style.cursor='e-resize';
+//   if(y > object.offsetHeight - 4)//position et cursor a modifier en fonction de la position
+//     object.style.cursor='n-resize';
+// if (x==posklex && y==poskley)
+//     object.style.cursor=cursor1
 
- }
-</script>  
+//  }
