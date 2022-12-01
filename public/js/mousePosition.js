c5af79e5e55fddac9aaedd19523ddb6979a745d1
--add <yassin.amazrayin.etu@univ-lille.fr>
2022-12-01 22:24:26 +0100

    maj code mousePositions

--- public/js/mousePosition.js
+++ public/js/mousePosition.js
@@ -12,9 +12,12 @@ document.onmousemove = function(e)
 
 function update(e)
 {
-  var x = xMousePos - object.offsetLeft;
-  var y = yMousePos - object.offsetTop;
-  alert("x=" + x + " y=" + y);
+if (klé== false){
+    alert("il faut la clé pour ouvrir);
+}
+if(klé== true )
+    //action pour la porte 
+    alert("la porte est ouverte")
 }
 
 object.onclick = update;
@@ -23,13 +26,11 @@ object.onmousemove = function(){
   var x = xMousePos - object.offsetLeft;
   var y = yMousePos - object.offsetTop;
   cursor1: url('...') x y, url('...') x y, défaut;// importer l'image d'une klé
-  object.style.cursor1='key'
-  if(x > object.offsetWidth - 4)
+  object.style.cursor='wait'
+  if(x > object.offsetWidth - 4)//position et cursor a modifier en fonction de la position
     object.style.cursor='e-resize';
-  if(y > object.offsetHeight - 4)
+  if(y > object.offsetHeight - 4)//position et cursor a modifier en fonction de la position
     object.style.cursor='n-resize';
-  //document.getElementById("message").innerHTML = "Pos x = " + x + "/" + 
-  // object.offsetWidth +  " y = " + y + 
-  //"/" +object.offsetHeight;
+
  }
 </script>  
